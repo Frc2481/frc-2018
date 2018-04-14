d03625ec97547fda9f74a772d625299ee761b4c4
Kython89 <kylelanman@gmail.com>
2018-04-14 08:10:14 -0500

    Observer logger and update to ntServer to use arrays.

--- ObserverPlotter/live/ntServer.py
+++ ObserverPlotter/live/ntServer.py
@@ -4,16 +4,26 @@ import time
 NetworkTables.initialize()
 sd = NetworkTables.getTable("SmartDashboard")
 
+ENABLED_FIELD = (1 << 0)
+AUTO_FIELD = (1 << 1)
+
+# Start logger
+fms = NetworkTables.getTable('FMSInfo')
+fms.putNumber('FMSControlData', ENABLED_FIELD | AUTO_FIELD)
+
 val = 0.0
-for i in range(1000):
-    sd.putNumber("Robot Pose X", val)
-    sd.putNumber("Robot Pose Y", val)
-    sd.putNumber("Robot Pose Theta", val)
-    sd.putNumber("PathX", val+30.0)
-    sd.putNumber("PathY", val+30.0)
-    sd.putNumber("Path Yaw", val+30.0)
+for i in range(200):
+
+    data = [val, val, val, val + 30, val + 30, val + 30, 0, 0, 0, 0, 0, 0, 30, 30, 30]
+    sd.putNumberArray("path_log_data", data)
+
     val += 0.3
     time.sleep(0.04)
     print i
+
+# Stop logger
+fms.putNumber('FMSControlData', 0)
+
+time.sleep(10)
 
 print "Done"
